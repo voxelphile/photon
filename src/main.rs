7f2992a78fcac23d097106ccdc3fd50750f33a55
Brynn Brancamp <brynnbrancamp@gmail.com>
2023-10-22 19:40:11 -0500

    Yo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use std::{net::{SocketAddr}, collections::{HashMap, HashSet}, time::Duration, env};
+#![feature(async_closure)]
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    hash::Hash,
+    net::SocketAddr,
+    time::Duration,
+};
 
 use futures::stream::FuturesUnordered;
+use hyper::{
+    header::HeaderValue, server::conn::Http, service::service_fn, HeaderMap, Method, Response,
+    StatusCode,
+};
 use shutil::pipe;
-use tokio::{net::{TcpListener, TcpStream, ToSocketAddrs}, io::copy_bidirectional};
+use tokio::{
+    io::copy_bidirectional,
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+};
 
 pub fn get_ip_of_pod_in_namespace(namespace: &str) -> Option<HashSet<String>> {
     //this only works on linux
@@ -17,10 +31,17 @@ pub fn get_ip_of_pod_in_namespace(namespace: &str) -> Option<HashSet<String>> {
         None?;
     }
 
-    let ips = output_result.unwrap().split("\n").map(|ip| ip.replace("IP:", "").trim().to_owned()).collect::<HashSet<_>>();
+    let ips = output_result
+        .unwrap()
+        .split("\n")
+        .map(|ip| ip.replace("IP:", "").trim().to_owned())
+        .collect::<HashSet<_>>();
 
     Some(ips)
 }
+
+pub type Host = &'static str;
+pub type Destination = &'static str;
 
 pub struct Router;
 
@@ -38,13 +59,89 @@ impl Router {
                     eprintln!("Failed to connect tcp");
                     continue;
                 };
-        
+
                 tokio::spawn(async move {
                     let Ok(_) = copy_bidirectional(&mut inbound, &mut outbound).await else {
                         eprintln!("failed to stream tcp data"); //lol
                         return;
                     };
                 });
+            }
+        });
+    }
+    pub async fn http<const N: usize>(
+        source: impl ToSocketAddrs,
+        routes: [(Host, Destination); N],
+    ) {
+        let Ok(listener) = TcpListener::bind(source).await else {
+            eprintln!("Failed to bind tcp");
+            return;
+        };
+        tokio::spawn(async move {
+            while let Ok((inbound, _)) = listener.accept().await {
+                if let Err(err) = Http::new()
+                .serve_connection(inbound, service_fn(async move |req: hyper::Request<hyper::Body>| -> Result<Response<String>, hyper::Error> {
+                    let Some((_, destination)) = routes.iter().find(|(host, _)| *host == req.headers().get("Host").unwrap().to_str().unwrap()) else {
+                        let mut resp = hyper::Response::new(String::default());
+                        resp.headers_mut().insert(hyper::header::LOCATION, HeaderValue::from_str("https://voxelphile.com/").unwrap());
+                        return Ok(resp);
+                    };
+
+                    let destination = destination.to_owned();
+
+                    let proxy_client = reqwest::Client::new();
+                    
+                    let proxy_req = match req.method() {
+                        &reqwest::Method::POST => proxy_client.post(destination),
+                        &reqwest::Method::GET => proxy_client.get(destination),
+                        &reqwest::Method::PUT => proxy_client.put(destination),
+                        &reqwest::Method::PATCH => proxy_client.patch(destination),
+                        &reqwest::Method::DELETE => proxy_client.delete(destination),
+                        _ => {
+                            let mut resp = hyper::Response::new(String::default());
+                            *resp.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+                            return Ok(resp);
+                        }
+                    };
+
+                    let mut proxy_headers = reqwest::header::HeaderMap::new();
+
+                    for (k, v) in req.headers() {
+                        proxy_headers.insert(k, v.clone());
+                    }
+
+                    let Ok(req_body) = String::from_utf8(hyper::body::to_bytes(req.into_body()).await?.to_vec()) else {
+                        let mut resp = hyper::Response::default();
+                        *resp.status_mut() = StatusCode::BAD_REQUEST;
+                        return Ok(resp);
+                    };
+
+                    let Ok(proxy_resp) = proxy_req.headers(proxy_headers).body(req_body).send().await else {
+                        let mut resp = hyper::Response::default();
+                        *resp.status_mut() = StatusCode::BAD_GATEWAY;
+                        return Ok(resp);
+                    };
+
+                    let mut resp = hyper::Response::new(String::new());
+
+                    *resp.status_mut() = proxy_resp.status();
+
+                    let mut resp_headers = hyper::HeaderMap::new();
+
+                    for (k, v) in proxy_resp.headers() {
+                        resp_headers.insert(k, v.clone());
+                    }
+
+                    *resp.headers_mut() = resp_headers;
+
+                    *resp.body_mut() = proxy_resp.text().await.unwrap_or_default();
+
+                    Ok(resp)
+                }))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
             }
         });
     }
